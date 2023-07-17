2071e6cffae13f6207ca1a6ac6a7913514a5368b
Eric de Vries <hro.mail@gmail.com>
2023-07-17 17:35:38 +0200

    Fix fs paths

--- src/test/java/nl/knaw/dans/vaultingest/core/inbox/MigrationIngestAreaTest.java
+++ src/test/java/nl/knaw/dans/vaultingest/core/inbox/MigrationIngestAreaTest.java
@@ -56,10 +56,10 @@ class MigrationIngestAreaTest {
     void validateDepositDir_should_work() throws Exception {
         var executor = Mockito.mock(ExecutorService.class);
         var process = Mockito.mock(DepositToBagProcess.class);
-        var outbox = new TestOutbox(Path.of("/outbox/path/"));
-        var area = new MigrationIngestArea(executor, process, Path.of("/input/path/"), outbox);
 
         try (var fs = MemoryFileSystemBuilder.newLinux().build()) {
+            var outbox = new TestOutbox(fs.getPath("/outbox/path/"));
+            var area = new MigrationIngestArea(executor, process, fs.getPath("/input/path/"), outbox);
             var path = fs.getPath("/input/path/batch1/deposit1");
             Files.createDirectories(path);
             Files.write(path.resolve("deposit.properties"), "state=FAILED".getBytes());
@@ -72,10 +72,11 @@ class MigrationIngestAreaTest {
     void validateBatchDirectory_should_handle_multiple_deposits() throws Exception {
         var executor = Mockito.mock(ExecutorService.class);
         var process = Mockito.mock(DepositToBagProcess.class);
-        var outbox = new TestOutbox(Path.of("/outbox/path/"));
-        var area = new MigrationIngestArea(executor, process, Path.of("/input/path/"), outbox);
 
         try (var fs = MemoryFileSystemBuilder.newLinux().build()) {
+            var outbox = new TestOutbox(fs.getPath("/outbox/path/"));
+            var area = new MigrationIngestArea(executor, process, fs.getPath("/input/path/"), outbox);
+
             var path = fs.getPath("/input/path/batch1/");
             Files.createDirectories(path.resolve("deposit1"));
             Files.createDirectories(path.resolve("deposit2"));
